b237fe43fca1f4edfb2c93a7bb95176f3088bd61
isreactive <97014592+isreactive@users.noreply.github.com>
2022-08-20 16:41:39 +0300

    new project

---
+++ src/main/java/com/example/cinemacity/CinemacityApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemacity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemacityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemacityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinemacity/CinemacityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemacity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemacityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
